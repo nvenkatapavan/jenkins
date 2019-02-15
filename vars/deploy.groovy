810baee045219fa718f36f48fb1d68291271b04e
Pavan <venkatpk@in.ibm.com>
2019-02-16 02:03:35 +0530

    asdfs

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -7,7 +7,8 @@ def call(Map config, Closure body) {
   config.debug = config.debug ?: false
   def logger = new OdsLogger(this, config.debug)
   def bp = new Deployer(this, config, logger)
-  return bp.getsecrets(body)
+  bp.getsecrets(body)
+  return this
 }
 
 return this
