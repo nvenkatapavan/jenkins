48ae99908a3b22e7b3cbcf5dbc4b31c55a697ce2
Pavan <venkatpk@in.ibm.com>
2019-02-16 00:43:37 +0530

    test

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,8 +1,25 @@
 #!/usr/bin/env groovy
 
+
+// define the secrets and the env variables
+def secrets = [
+    [$class: 'VaultSecret', path: 'secret/data/hello', secretValues: [
+        [$class: 'VaultSecretValue', envVar: 'testing', vaultKey: 'mysecret'],
+]]]
+// optional configuration, if you do not provide this the next higher configuration
+// (e.g. folder or global) will be used
+def configuration = [$class: 'VaultConfiguration',
+                        vaultUrl: 'http://host.docker.internal:8200',
+                        vaultCredentialId: 'vault-token']
+def getsecrets() {
+    // inside this block your credentials will be available as env variables
+    wrap([$class: 'VaultBuildWrapper', configuration: configuration, vaultSecrets: secrets]) {
+        script.echo ${testing}
+    }   
+
 def call(body) {
     echo "Start Deploy"
-    new org.foo.Deployer.getsecrets()
+    getsecrets()
     echo "Deployed"
     currentBuild.result = 'SUCCESS'
     return this
